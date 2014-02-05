75c000b60813233305613558a1eddbb1cd9af97c
Nick Funnell <nick@nick.gs>
2014-02-05 05:01:43 +0000

    drop debug printing"

--- 15/15.py
+++ 15/15.py
@@ -4,10 +4,8 @@ sides = 5
 def paths(x, y, dist, goal):
     if dist==goal:
         if x==0 and y==0:
-            print "Path"
             return 1
         else:
-            print "Bad Path"
             return 0
     if x>0 and y>0:
         return paths(x-1,y,dist+1,goal) + paths(x,y-1,dist+1,goal)
